3a9a73405b000d24a625ca66d4987cb7f99f618d
Toni Lee <contact.tonilee@gmail.com>
2016-06-15 14:56:05 +0200

    as

--- DoD/Game.cs
+++ DoD/Game.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleFunctions;
 
 namespace DungeonsOfDoom
 {
@@ -28,6 +29,7 @@ namespace DungeonsOfDoom
         //Public Methods
         public void Play()
         {
+            AsciiArt
             do
             {
                 Console.Clear();
